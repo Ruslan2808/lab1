62c2ca5e751bfefddd277c10a2c77f9adcb0972e
Ruslan2808 <71653380+Ruslan2808@users.noreply.github.com>
2020-09-22 12:43:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,14 @@
 using namespace std;
 
 int main() {
-	setlocale(LC_ALL, "rus");
 	int n;
-	cout << "Введите размер матрицы: ";
+	cout << "Enter size matrix: ";
 	cin >> n;
 	int** mas = new int* [n];
 	for (int i = 0; i < n; i++) {
 		mas[i] = new int[n];
 	}
-	cout << "Введите элементы матрицы : " << endl;
+	cout << "Enter matrix elements: " << endl;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> mas[i][j];
@@ -28,7 +27,7 @@ int main() {
 			}
 		}
 	}
-	cout << endl << "Матрица: ";
+	cout << endl << "Matrix: ";
 	for (int i = 0; i < n; i++) {
 		cout << endl;
 		for (int j = 0; j < n; j++) {
